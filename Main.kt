7dc2dbd65fccdf7133677cea5081551aa67bfb1e
yvonne <yvonnemartinkatumbi@gmail.com>
2024-02-06 23:47:43 +0300

    add file

---
+++ Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var studentsName = "yvonne martin"
+    println(studentsName)
+    var age = 22
+    println(age)
+    var phoneNumber = +25492693383
+    println(phoneNumber)
+    var weight = 67.3
+    println(weight)
+    var nationality = "kenyan"
+    println(nationality)
+    val citizen = true
+    val noncitizen = false
+
+
+}
